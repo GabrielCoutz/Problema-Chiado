5c04ce463191e1942eeaf537b1769185fe29a893
Gabriel Coutinho Cassiano <86968853+GabrielCoutz@users.noreply.github.com>
2021-07-12 11:11:22 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Problema Chiado',
+    packages=['pywin32,psutil,PyGetWindow,PySimpleGUI'],
+    version='1',  # Ideally should be same as your GitHub release tag varsion
+    description='Descrição teste',
+    author='Gabriel Coutinho',
+    author_email='teste@gmail.com',
+    url='github package source url',
+    download_url='download link you saved',
+    keywords=['tag1', 'tag2'],
+    classifiers=[],
+)
